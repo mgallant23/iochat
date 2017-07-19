ab6ece1abd630e90ca64cc5d9d3676ada8f1119c
Michael Gallant <michaelgallant@Michaels-MacBook-Pro.local>
2017-07-19 00:25:16 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+
+console.log('server running');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+ connections.push(socket);
+ console.log('Connected: %s sockets connected', connections.length);
+
+ socket.on('disconnect',function(data){
+ users.splice(users.indexOf(socket.username),1);
+ updateUsernames();
+ connections.splice(connections.indexOf(socket),1);
+ console.log('Disconnected: %s sockets conected', connections.length);
+});
+ //send message
+ socket.on('send message', function(data){
+  console.log(data)
+  io.sockets.emit('new message',{msg: data, user: socket.username});
+ });
+});
